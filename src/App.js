3a07753ef65911bedc0dcfa622a8463f8b33a337
Metoosky00 <malfavontalaveraj@hotmail.com>
2020-05-07 15:28:44 -0500

    Comenzamos la pagina
    en este commit se crea la estructura del header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Grid, Row, Col } from "react-flexbox-grid2";
 import "./main.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Grid className="grid">
+      <Row xs={12} className="row">
+        <div className="header">
+          <div className="header__login">login</div>
+          <div className="header__navigation">navegacion</div>
+        </div>
+      </Row>
+      <Row xs={12}>Main</Row>
+      <Row xs={12}>footer</Row>
+    </Grid>
+  );
 }
 
 export default App;
